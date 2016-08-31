fd800e76ea31cd5d4979efe1161d71664ebfcecf
iamborisov <371rus>
2016-08-31 18:34:13 +0200

    mobile markup - input fixes

--- widgets/views/answerEditor/select_multiple.php
+++ widgets/views/answerEditor/select_multiple.php
@@ -6,8 +6,8 @@
         <div class="items"></div>
     </div>
     <div class="template item-template item row">
-        <div class="col-md-1"><input type="checkbox" class="pull-right"></div>
-        <div class="col-md-10"><span class="text"></span></div>
+        <div class="col-xs-2 col-md-1"><input type="checkbox" class="pull-right"></div>
+        <div class="col-xs-10 col-md-11"><span class="text"></span></div>
     </div>
 </div>
 
--- widgets/views/questionEditor/select_one.php
+++ widgets/views/questionEditor/select_one.php
@@ -12,9 +12,9 @@
         </div>
     </div>
     <div class="template item-template item row">
-        <div class="col-md-1"><input type="radio" name="<?= $id ?>" class="pull-right"></div>
-        <div class="col-md-10"><input type="text" class="form-control"></div>
-        <div class="col-md-1"><a class="btn btn-danger btn-xs remove pull-left">Удалить</a></div>
+        <div class="col-xs-2 col-md-1"><input type="radio" name="<?= $id ?>" class="pull-right"></div>
+        <div class="col-xs-8 col-md-10"><input type="text" class="form-control"></div>
+        <div class="col-xs-2 col-md-1"><a class="btn btn-danger btn-xs remove pull-left">Удалить</a></div>
     </div>
 </div>
 

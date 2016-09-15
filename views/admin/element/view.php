331311e8239de392b1b6745fc1c494f3d17d8e9a
iamborisov <371rus>
2016-09-15 14:34:05 +0200

    #4 Display "Create another" button after question creation

--- views/admin/element/view.php
+++ views/admin/element/view.php
@@ -16,6 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
+        <?= Html::a(Yii::t('app', 'Create'), ['create'], ['class' => 'btn btn-success']) ?>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
